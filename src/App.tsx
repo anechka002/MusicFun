89be5ac3a94301223a35df83b60022459320515a
Anna <blinova.anneta@gmail.com>
2025-08-22 13:24:28 +0200

    `Update App component to fetch TracksResponse data and render tracks in a list`

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from 'react';
-import type { TrackDataItem } from './types/types';
+import type { TrackDataItem, TracksResponse } from './types/types';
 
 export function App() {
   const [tracks, setTracks] = useState<TrackDataItem[]>([]);
 
   useEffect(() => {
-    // rest api
     fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
       headers: {
         'API-KEY': '2379558b-0188-43ee-9a39-5ee90ce1492e',
       },
     })
-      .then((res) => res.json())
+      .then((res) => res.json() as Promise<TracksResponse>)
       .then((data) => {
         console.log(data);
-        setTracks(data.data)
+        setTracks(data.data);
       });
   }, []);
 
-  const trackElement = tracks.map((track, index) => (
-    <li key={index}>
-      <h5>{track.attributes.title}</h5>
-      <audio src={track.attributes.attachments[0].url} controls={true}></audio>
-    </li>
-  ));
-
-  return <ul>{trackElement}</ul>;
+  return (
+    <div>
+      <h2>Music Fun</h2>
+      <ul>
+        {tracks.map((track, index) => (
+          <li key={index}>
+            <h5>{track.attributes.title}</h5>
+            <audio
+              src={track.attributes.attachments[0].url}
+              controls={true}
+            ></audio>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
