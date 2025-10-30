7b3f637101e4b0414ae758f49e38c7c5dedef4ac
Anna <blinova.anneta@gmail.com>
2025-10-30 11:07:09 +0100

    `feat: added logout functionality`

--- src/routing/layouts/AuthLayout.tsx
+++ src/routing/layouts/AuthLayout.tsx
@@ -1,6 +1,8 @@
 import {NavLink, type NavLinkRenderProps, Outlet} from "react-router";
 import s from './Layout.module.css'
-import {useMeQuery} from "@/features-layer/auth-slice/model/useMeQuery.tsx";
+import {useMeQuery} from "@/features-layer/auth-slice/model/useMeQuery.ts";
+import {useQueryClient} from "@tanstack/react-query";
+import {storage} from "@/shared-layer/libs-segment/storage.ts";
 
 export const AuthLayout = () => {
 
@@ -8,6 +10,14 @@ export const AuthLayout = () => {
     isActive ? s.active : ""
 
   const {data, isLoading, isError, error} = useMeQuery()
+  const queryClient = useQueryClient();
+
+  const handleLogout = () => {
+    storage.removeBasicCredentials()
+    queryClient.resetQueries({
+      queryKey: ['auth']
+    })
+  }
 
   return (
     <div>
@@ -16,13 +26,13 @@ export const AuthLayout = () => {
         {!data && !isLoading && (
           <NavLink to={"/auth/login"} className={renderClassName}>Login</NavLink>
         )}
-        {/*{" | "}*/}
         {isError && (
           <NavLink to={"/auth/register"} className={renderClassName}>Register</NavLink>
         )}
       </nav>
       {error && <span>{JSON.stringify(error)}</span>}
       {data && <NavLink to={"/profile/" + data.userId} className={renderClassName}>{data.login}</NavLink>}
+      {data && <button onClick={handleLogout}>Logout</button>}
       <Outlet />
     </div>
   );
